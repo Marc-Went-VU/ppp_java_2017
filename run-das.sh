8a841c0094dbbabb801f7701aa02d05a8c58c832
Marc Went <mwt680@fs0.cm.cluster>
2018-01-16 14:43:44 +0100

    Add run-das.sh script

---
+++ run-das.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ant 
+POOLSIZE=$1
+POOLNAME=pool-$(date +%Y%m%d%H%M%S)
+
+prun -v -1 -np $POOLSIZE bin/java-run -Dibis.pool.name=$POOLNAME -Dibis.pool.size=$POOLSIZE -Dibis.server.address=fs0:4321 ida.ipl.Ida $@
+
+wait
