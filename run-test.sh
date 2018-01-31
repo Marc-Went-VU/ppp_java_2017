37d971d52bac6d7d568eb2f153f6f5809f748b2b
Marc Went <mwent@live.nl>
2018-01-31 18:07:13 +0100

    update the test script

--- run-test.sh
+++ run-test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-ant 
+ant
 
 POOLSIZE=8
 POOLNAME=pool-$(date +%Y%m%d%H%M%S)-$(date +%N)
 
 for (( i=0; i < $POOLSIZE; i++ )); do
-	./bin/java-run -Dibis.pool.name=$POOLNAME -Dibis.pool.size=$POOLSIZE -Dibis.server.address=localhost:4321 ida.test.Ida $@ &
+	./bin/java-run -Dibis.pool.name=$POOLNAME -Dibis.pool.size=$POOLSIZE -Dibis.server.address=localhost:4321 ida.ipl.Ida $@ &
 done
 
 wait
